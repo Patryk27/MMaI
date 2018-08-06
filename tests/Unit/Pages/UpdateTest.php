d5e8954e5774e42d869b8c04b85ee5db7112bfce
Patryk Wychowaniec <wychowaniec.patryk@gmail.com>
2018-08-06 17:18:37 +0200

    [tests] Finish "Unit\Pages\UpdateTest".

--- tests/Unit/Pages/UpdateTest.php
+++ tests/Unit/Pages/UpdateTest.php
@@ -148,7 +148,8 @@ class UpdateTest extends TestCase
     }
 
     /**
-     * This test makes sure that the update() method @todo
+     * This test makes sure that the update() method sets the "published at"
+     * to current date & time when page is being published.
      *
      * @return void
      *
@@ -156,32 +157,169 @@ class UpdateTest extends TestCase
      */
     public function testSetsPublishedAt(): void
     {
+        $this->pagesFacade->update($this->page, [
+            'pageVariants' => [
+                [
+                    'id' => $this->pageVariant->id,
+                    'route' => '/somewhere',
+                    'status' => PageVariant::STATUS_PUBLISHED,
+                ]
+            ],
+        ]);
 
+        $this->assertEquals(PageVariant::STATUS_PUBLISHED, $this->pageVariant->status);
+        $this->assertNotNull($this->pageVariant->published_at);
     }
 
+    /**
+     * This test makes sure that the update() method creates a new route when
+     * told to.
+     *
+     * @return void
+     *
+     * @throws AppException
+     */
     public function testCreatesRoute(): void
     {
+        $this->pagesFacade->update($this->page, [
+            'pageVariants' => [
+                [
+                    'id' => $this->pageVariant->id,
+                    'route' => '/somewhere',
+                ]
+            ],
+        ]);
 
+        $this->assertNotNull($this->pageVariant->route);
+        $this->assertEquals('/somewhere', $this->pageVariant->route->url);
     }
 
+    /**
+     * This test makes sure that the update() method updates an already existing
+     * route when told to.
+     *
+     * @return void
+     *
+     * @throws AppException
+     */
     public function testUpdatesRoute(): void
     {
+        // Step 1: create a new route (so that we will be able to update it
+        // later)
+        $this->pagesFacade->update($this->page, [
+            'pageVariants' => [
+                [
+                    'id' => $this->pageVariant->id,
+                    'route' => '/somewhere',
+                ]
+            ],
+        ]);
 
+        // Step 2: remove that route
+        $this->pagesFacade->update($this->page, [
+            'pageVariants' => [
+                [
+                    'id' => $this->pageVariant->id,
+                    'route' => '/somewhere-else',
+                ]
+            ],
+        ]);
+
+        $this->assertNotNull($this->pageVariant->route);
+        $this->assertEquals('/somewhere-else', $this->pageVariant->route->url);
     }
 
+    /**
+     * This test makes sure that the update() method removes an already existing
+     * route when told to.
+     *
+     * @return void
+     *
+     * @throws AppException
+     */
     public function testDeletesRoute(): void
     {
+        // Step 1: create a new route (so that we will be able to remove it
+        // later)
+        $this->pagesFacade->update($this->page, [
+            'pageVariants' => [
+                [
+                    'id' => $this->pageVariant->id,
+                    'route' => '/somewhere',
+                ]
+            ],
+        ]);
 
+        // Step 2: remove that route
+        $this->pagesFacade->update($this->page, [
+            'pageVariants' => [
+                [
+                    'id' => $this->pageVariant->id,
+                    'route' => '',
+                ]
+            ],
+        ]);
+
+        $this->assertNull($this->pageVariant->route);
     }
 
+    /**
+     * This test makes sure it is not possible to publish a page that does not
+     * have any route.
+     *
+     * @return void
+     *
+     * @throws AppException
+     */
     public function testForbidsToPublishPageWithoutRoute(): void
     {
+        $this->expectExceptionMessage('Published page must have a route.');
 
+        $this->pagesFacade->update($this->page, [
+            'pageVariants' => [
+                [
+                    'id' => $this->pageVariant->id,
+                    'status' => PageVariant::STATUS_PUBLISHED,
+                ],
+            ],
+        ]);
     }
 
+    /**
+     * This test makes sure it is not possible not publish a post that does not
+     * have any route.
+     *
+     * @return void
+     *
+     * @throws AppException
+     */
     public function testForbidsToPublishPostWithoutLead(): void
     {
-        
+        $this->expectExceptionMessage('Published post must contain a lead.');
+
+        // Step 1: create an example post
+        $page = $this->pagesFacade->create([
+            'page' => [
+                'type' => Page::TYPE_BLOG,
+            ],
+
+            'pageVariants' => [
+                []
+            ],
+        ]);
+
+        $pageVariant = $page->pageVariants[0];
+
+        // Step 2: try to publish it
+        $this->pagesFacade->update($page, [
+            'pageVariants' => [
+                [
+                    'id' => $pageVariant->id,
+                    'route' => '/somewhere',
+                    'status' => PageVariant::STATUS_PUBLISHED,
+                ]
+            ],
+        ]);
     }
 
 }
