075a52368bebaa156691deb4b92b0767348307c2
Patryk Wychowaniec <wychowaniec.patryk@gmail.com>
2018-10-13 17:19:51 +0200

    Get rid of unnecessary comment.

--- resources/views/backend/pages/tags/search/columns/name.blade.php
+++ resources/views/backend/pages/tags/search/columns/name.blade.php
@@ -5,9 +5,5 @@
 @endphp
 
 <span data-column="name" data-tag="{{ $row }}">
-    {{--<button type="button" class="btn btn-sm btn-icon-only btn-primary"> @todo make this button great again --}}
-    {{--<i class="fa fa-edit"></i>--}}
-    {{--</button>--}}
-
     {{ $row->name }}
 </span>
