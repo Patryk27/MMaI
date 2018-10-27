eb22980159a460eaac7914e211739dfc27bda2a9
Patryk Wychowaniec <wychowaniec.patryk@gmail.com>
2018-10-27 17:17:02 +0200

    Fix a NPE in the `backend.components.pages.table.actions` view.

--- resources/views/backend/components/pages/table/actions.blade.php
+++ resources/views/backend/components/pages/table/actions.blade.php
@@ -5,9 +5,11 @@
 @endphp
 
 <div data-column="actions">
-    <a class="btn btn-sm btn-success btn-icon-only" href="{{ $row->route->getTargetUrl() }}">
-        <i class="fa fa-link"></i>
-    </a>
+    @isset($row->route)
+        <a class="btn btn-sm btn-success btn-icon-only" href="{{ $row->route->getTargetUrl() }}">
+            <i class="fa fa-link"></i>
+        </a>
+    @endisset
 
     <a class="btn btn-sm btn-primary btn-icon-only" href="{{ $row->getBackendEditUrl() }}">
         <i class="fa fa-edit"></i>
